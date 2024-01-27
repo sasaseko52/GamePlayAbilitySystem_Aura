27b3d28c886fa981b7257f0b5c93f917f0974784
sasaseko52 <sasaseko52@gmail.com>
2024-01-27 19:57:42 +0200

    The Base Character Class

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Dark Sides
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/Aura/Public/AuraCharacterBase.h
@@ -0,0 +1,29 @@
+// Copyright Dark Sides
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	AAuraCharacterBase();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+public:	
+	
+	virtual void Tick(float DeltaTime) override;
+
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
