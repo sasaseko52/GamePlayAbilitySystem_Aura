b5fb7395156c279d2752ac14f7f8502f63109b78
sasaseko52 <sasaseko52@gmail.com>
2024-01-28 20:40:57 +0200

    Aura Animations BP And Player Controller

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,23 @@
+// Copyright Dark Sides
+
+
+#include "Player/AuraPlayerController.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates= true;
+
+	
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+
+
+
+	
+}
