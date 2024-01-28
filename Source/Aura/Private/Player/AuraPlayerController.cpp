e1194668399d37ea95d991ce504e1e14cbaee8f8
sasaseko52 <sasaseko52@gmail.com>
2024-01-28 21:29:26 +0200

    Enhanced Input Subsystem

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,7 +2,7 @@
 
 
 #include "Player/AuraPlayerController.h"
-
+#include "EnhancedInputSubsystems.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -14,10 +14,17 @@ AAuraPlayerController::AAuraPlayerController()
 void AAuraPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	check(AuraContext); /// checking if this ptr is valid before player the game
 
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext,0);
+
+	DefaultMouseCursor = EMouseCursor::Hand;
+	bShowMouseCursor = true;
 	
-
-
-
-	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
 }
